e0f5f5290433a75950a3182f1ea8acd5a56dc88e
sapient-nirav <nirav.rathod@sapientcodelabs.xyz>
2024-01-23 13:16:42 +0530

    expense-tracker

--- 10-expense/src/components/Header.js
+++ 10-expense/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h2>Header</h2>
+    <h2 className="mx-auto m w-[350px] text-2xl font-bold my-3">Manage Your Expense Here</h2>
   )
 }
 
